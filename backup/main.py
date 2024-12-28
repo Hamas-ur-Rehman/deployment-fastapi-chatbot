126c914f080ab7a9696f82911237d8c5eb347ebe
hamasurrehman <hamasurrehman@gmail.com>
2024-12-28 20:09:21 +0500

    data saved

--- backup/main.py
+++ backup/main.py
@@ -1,31 +1,31 @@
-from fastapi import FastAPI, HTTPException
-from fastapi.responses import JSONResponse, StreamingResponse
-from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import StreamingResponse, JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from example1 import chain
 
 class QueryInput(BaseModel):
-    question:str
-    stream:bool = False
+    question: str
+    stream: bool = False
+
 
 app = FastAPI()
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"]
+    CORSMiddleware
+    , allow_origins=["*"]
+    , allow_methods=["*"]
+    ,allow_headers=["*"]
 )
 
-
 @app.post("/ask")
-def handle_question(input: QueryInput):
+def handle_question(input: QueryInput): 
     try:
-        if input.stream == True:
+        if input.stream:
             response = chain.stream({"question":input.question})
             return StreamingResponse(response)
         else:
             response = chain.invoke({"question":input.question})
             return JSONResponse(content={"response":response})
     except Exception as e:
-        raise HTTPException(status_code=500,detail=str(e))
+        raise HTTPException(status_code=500, detail=str(e))
--- example1.py
+++ example1.py
@@ -1,65 +1,3 @@
-# # chat service with chroma
-# import os
-# import shutil
-# from dotenv import load_dotenv
-# from langchain_chroma import Chroma
-# from langchain_openai import OpenAIEmbeddings,ChatOpenAI
-# from langchain_core.output_parsers import StrOutputParser
-# from langchain_core.runnables import RunnablePassthrough
-# from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-# load_dotenv()
-
-
-
-# def load_chunks(docs):
-#     embeddings = OpenAIEmbeddings()
-#     vectorstore = Chroma(
-#         persist_directory="./chromadb",
-#         embedding_function=embeddings,
-#         collection_name="test_collection",
-#     )
-#     print("Adding documents to vectorstore")
-#     Chroma.add_documents(vectorstore, docs)
-#     print("Documents added to vectorstore")
-
-# def get_retriver():
-#     embeddings = OpenAIEmbeddings(
-#         model='text-embedding-ada-002'
-#     )
-#     vectorstore = Chroma(
-#         persist_directory="./chromadb",
-#         embedding_function=embeddings,
-#         collection_name="test_collection",
-#     )
-
-#     return vectorstore.as_retriever()
-
-# def format_docs(docs):
-#     return "\n\n".join(doc.page_content for doc in docs)
-
-
-
-
-# LLM = ChatOpenAI(model='gpt-4o-mini')
-# retriver = get_retriver()
-# prompt = ChatPromptTemplate.from_template(
-#     """You can answer any question from this data
-#     {data}
-
-#     This is the question: {question}
-#     """
-# )
-# chain = ( 
-#     {"data": retriver | format_docs, "question": RunnablePassthrough()}
-#     | prompt 
-#     | LLM 
-#     | StrOutputParser())
-
-# print(chain.invoke("Hello"))
-
-
-
-
 import os 
 from dotenv import load_dotenv
 
@@ -78,5 +16,4 @@ prompt = ChatPromptTemplate.from_template(
     "You can joke and talk to the user this is the user question\n: {question}"
     )
 chain = prompt | model | StrOutputParser()
-# print(chain.invoke({"question": "bears"}))
 
